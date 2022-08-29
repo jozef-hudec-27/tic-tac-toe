1eb536b87ccaa532c1d54b4f8816a96eed516403
Jozef Hudec <jojohudec27@gmail.com>
2022-08-29 12:27:07 +0200

    Add tests for #refresh + improve some variable names

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,47 +3,47 @@ require_relative '../game'
 describe Board do
     describe '#check_for_winner' do
         context 'when there is a horizontal/vertical winner' do
-            subject(:horizontal_winner_board) { described_class.new([['❌', '❌', '❌'], [4, 5, 6], [7, 8, 9]]) }
-            subject(:vertical_winner_board) { described_class.new([[1, '🟢', 3], [4, '🟢', 6], [7, '🟢', 9]]) }
+            subject(:board_horizontal_winner) { described_class.new([['❌', '❌', '❌'], [4, 5, 6], [7, 8, 9]]) }
+            subject(:board_vertical_winner) { described_class.new([[1, '🟢', 3], [4, '🟢', 6], [7, '🟢', 9]]) }
             
             it "returns the winner's symbol" do 
-                expect(horizontal_winner_board.check_for_winner).to eql('❌')
+                expect(board_horizontal_winner.check_for_winner).to eql('❌')
             end
 
             it "returns the winner's symbol" do 
-                expect(vertical_winner_board.check_for_winner).to eql('🟢')
+                expect(board_vertical_winner.check_for_winner).to eql('🟢')
             end
         end
 
         context 'when there is a diagonal winner' do 
-            subject(:diagonal_winner_board1) { described_class.new([['❌', 2, 3], [4, '❌', 6], [7, 8, '❌']]) }
-            subject(:diagonal_winner_board2) { described_class.new([[1, 2, '🟢'], [4, '🟢', 6], ['🟢', 8, 9]]) }
+            subject(:board_diagonal_winner1) { described_class.new([['❌', 2, 3], [4, '❌', 6], [7, 8, '❌']]) }
+            subject(:board_diagonal_winner2) { described_class.new([[1, 2, '🟢'], [4, '🟢', 6], ['🟢', 8, 9]]) }
 
             it "returns the winner's symbol" do
-                expect(diagonal_winner_board1.check_for_winner).to eql('❌')
+                expect(board_diagonal_winner1.check_for_winner).to eql('❌')
             end
 
             it "returns the winner's symbol" do
-                expect(diagonal_winner_board2.check_for_winner).to eql('🟢')
+                expect(board_diagonal_winner2.check_for_winner).to eql('🟢')
             end
         end
 
         context 'when there is no winner' do
-            subject(:empty_board) { described_class.new }
-            subject(:full_board) { described_class.new([['❌', '🟢', '❌'], ['🟢', '❌', '🟢'], ['🟢', '❌', '🟢']]) }
+            subject(:board_empty) { described_class.new }
+            subject(:board_full) { described_class.new([['❌', '🟢', '❌'], ['🟢', '❌', '🟢'], ['🟢', '❌', '🟢']]) }
 
             it 'returns nil' do
-                expect(empty_board.check_for_winner).to be_nil
+                expect(board_empty.check_for_winner).to be_nil
             end
 
             it 'returns nil' do
-                expect(full_board.check_for_winner).to be_nil
+                expect(board_full.check_for_winner).to be_nil
             end
         end
     end
 
     describe '#tie?' do
-        context 'the board is full and there is no winner' do
+        context 'when the board is full and there is no winner' do
             subject(:board_tie) { described_class.new([['❌', '🟢', '❌'], ['🟢', '❌', '🟢'], ['🟢', '❌', '🟢']]) }
 
             it 'returns true' do
@@ -63,6 +63,25 @@ describe Board do
             subject(:board_full_with_winner) { described_class.new([['❌', '🟢', '❌'], ['🟢', '❌', '🟢'], ['🟢', '❌', '❌']])}
             it 'returns false' do 
                 expect(board_full_with_winner).to_not be_tie
+            end
+        end
+    end
+
+    describe '#refresh' do
+        context 'when the board is empty' do
+            subject(:board_empty) { described_class.new }
+
+            it 'stays empty' do
+                expect { board_empty.refresh }.to_not change { board_empty.board }
+            end
+        end
+
+        context 'when the board is mid game' do
+            subject(:board_mid_game) { described_class.new([[1, 2, '🟢'], [4, '❌', 6], ['🟢', 8, 9]]) }
+            let(:board_empty_array) { [[1, 2, 3], [4, 5, 6], [7, 8, 9]] }
+
+            it 'becomes empty' do
+                expect { board_mid_game.refresh }.to change { board_mid_game.board }.to(board_empty_array)
             end
         end
     end
