9dea84be41428923017471bffc2a8f7173b17b76
Jozef Hudec <jojohudec27@gmail.com>
2022-08-29 11:54:15 +0200

    Allow users to create an instance of Board with custom board property

--- game.rb
+++ game.rb
@@ -18,8 +18,8 @@ end
 class Board
   attr_accessor :board
 
-  def initialize
-    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  def initialize(board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    @board = board
   end
 
   def check_for_winner
